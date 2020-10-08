990ac192dacf5266c4771e13b8fa88cd6ff9695c
fredg1 <gagnonfrederic9@gmail.com>
2020-10-08 18:36:33 -0400

    Update Compile_ASH_script.js

--- .github/actions/Compile_ASH_script.js
+++ .github/actions/Compile_ASH_script.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 console.log(`github.workspace : ${ github.workspace }`);
-# console.log(`env.GITHUB_WORKSPACE : ${ env.GITHUB_WORKSPACE }`);
+// console.log(`env.GITHUB_WORKSPACE : ${ env.GITHUB_WORKSPACE }`);
 console.log(`github.repository : ${ github.repository }`);
 const time = (new Date()).toTimeString();
 core.setOutput("compiled_output", time);
